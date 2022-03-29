56b92c0c7b8dc8effb2086db14eb43db0b57225d
LeonardoFerraz25 <leonardo.ferrazn@gmail.com>
2022-03-28 22:37:21 -0300

    creat section promoçoes

--- script.js
+++ script.js
@@ -1,6 +1,31 @@
 const openCart = document.querySelector('#openCart');
+const forward = document.querySelector('.forward');
+const backward = document.querySelector('.backward');
+const slider = document.querySelector('.scroll');
+let scrollAmount = 0;
 
 openCart.addEventListener('click', () => {
   document.querySelector('.container-cartTitle').classList.toggle('show-title');
   openCart.classList.toggle('visible');
 });
+
+backward.addEventListener('click', () => {
+  slider.scrollTo({
+    top: 0 ,
+    left: (scrollAmount -= 1000),
+    behavior: "smooth"
+  });
+  if (scrollAmount < 0) {
+    scrollAmount = 0
+  }
+})
+
+forward.addEventListener('click', () => {
+  if(scrollAmount <= slider.scrollWidth - slider.clientWidth) {
+    slider.scrollTo({
+      top: 0,
+      left: (scrollAmount += 1000),
+      behavior: "smooth"
+    })
+  }
+})

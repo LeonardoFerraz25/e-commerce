2e1beee9af9f25080a715f4a7ae1439abcb5df98
LeonardoFerraz25 <leonardo.ferrazn@gmail.com>
2022-03-29 17:20:46 -0300

    add funçoes dos cards

---
+++ helpers/callProducts.js
@@ -0,0 +1,54 @@
+const ferramentas = document.querySelector('#ferramentas');
+ferramentas.addEventListener('click', () => {
+  createProductList('ferramentas');
+});
+
+const eletrodomesticos = document.querySelector('#eletrodomesticos');
+eletrodomesticos.addEventListener('click', () => {
+  createProductList('eletrodomesticos');
+})
+
+const moda = document.querySelector('#moda');
+moda.addEventListener('click', () => {
+  createProductList('moda');
+})
+
+const informatica = document.querySelector('#informatica');
+informatica.addEventListener('click', () => {
+  createProductList('informatica');
+})
+
+const esportes = document.querySelector('#esportes');
+esportes.addEventListener('click', () => {
+  createProductList('esportes');
+})
+
+const veiculos = document.querySelector('#veiculos');
+veiculos.addEventListener('click', () => {
+  createProductList('veiculos');
+})
+
+const ofertas = document.querySelector('#ofertas');
+ofertas.addEventListener('click', () => {
+  createProductList('ofertas');
+})
+
+const eletro = document.querySelector('#eletro');
+eletro.addEventListener('click', () => {
+  createProductList('eletro');
+})
+
+const roupa = document.querySelector('#roupa');
+roupa.addEventListener('click', () => {
+  createProductList('roupa');
+})
+
+const infor = document.querySelector('#infor');
+infor.addEventListener('click', () => {
+  createProductList('informatica');
+})
+
+const carros = document.querySelector('#carros');
+carros.addEventListener('click', () => {
+  createProductList('carros');
+})
